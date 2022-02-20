05c1ed2fcf5ef0d6fedd066232528828793f6f59
Aadarsh Senthil <aadhisen2006@gmail.com>
2022-02-20 12:51:03 -0500

    helper function for drop freight in rr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/RoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/RoadRunnerTest.java
@@ -6,17 +6,21 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.TestPrograms.Carousel;
 import org.firstinspires.ftc.teamcode.TestPrograms.CyberDragonsOpModeTemplate;
+import org.firstinspires.ftc.teamcode.TestPrograms.ShippingHubAutomationLevels;
 
 @Autonomous
 public class RoadRunnerTest extends LinearOpMode {
 
     public void runOpMode() throws InterruptedException {
 
-        org.firstinspires.ftc.teamcode.TestPrograms.Carousel carousel = new Carousel();
+        Carousel carousel = new Carousel();
 
-        org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive drive = new org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive(hardwareMap);
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        ShippingHubAutomationLevels shippingHubAutomationLevels = new ShippingHubAutomationLevels();
 
         Pose2d startPose = new Pose2d(-35, 65, Math.toRadians(-90));
 
@@ -38,11 +42,14 @@ public class RoadRunnerTest extends LinearOpMode {
 
         if (opModeIsActive()) {
 
-            drive.followTrajectory(carouselTurner);
             // do carousel
+            drive.followTrajectory(carouselTurner);
             carousel.deliverDuck();
-            drive.followTrajectory(shippingHub);
+
             // place freight
+            drive.followTrajectory(shippingHub);
+            shippingHubAutomationLevels.dropFreightInLevel(3);
+            
             //drive.turn(Math.toRadians(90));
             //drive.followTrajectory(parkWarehouse);
 
