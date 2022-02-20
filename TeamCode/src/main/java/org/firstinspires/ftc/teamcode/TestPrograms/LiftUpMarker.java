4a7badf796bbeb4f776d74f1bc6ec3f6091fea94
Brendan D <brendandacunha@gmail.com>
2022-02-20 15:17:36 -0500

    made liftupmarker and added the code for that

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPrograms/LiftUpMarker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPrograms/LiftUpMarker.java
@@ -1,4 +1,18 @@
-package org.firstinspires.ftc.teamcode;
+/*
+TeleOp:
+1. Get infront of Team Marker
+2. Press button or somehow changes to Autonomous
+Autonomous:
+1. Backup a enough to put the arm down
+2. Move arm all the way down (Or enough to pick it up)
+3. Move forward same amount as Part 1 or enough to connect to the hook thing
+4. Move arm up to the top of the shipping area
+5. Lower it enough so it touches the pole, and enough so it un-hooks
+6. Move Back
+7. Switch to TeleOp/End function
+*/
+
+package org.firstinspires.ftc.teamcode.TestPrograms;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -13,15 +27,25 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous
 public class DropOffMarker extends LinearOpMode {
   //Motors
-	private DcMotorEx frontLeft;
-	private DcMotorEx frontRight;
-	private DcMotorEx backLeft;
-	private DcMotorEx backRight;
+  private DcMotorEx frontRight;
+  private DcMotorEx frontLeft;
+  private DcMotorEx backLeft;
+  private DcMotorEx backRight;
 
   //Arms/Picking Things Up
-	private DcMotorEx bucket;
-	private DcMotorEx bucketTurner;
-	private DcMotorEx armMotor;
+  private DcMotorEx bucket;
+  private DcMotorEx bucketTurner;
+  private DcMotorEx armMotor;
+
+  //Sensors
+  private DistanceSensor distanceLeftFront;
+  private DistanceSensor distanceLeftBack;
+  private DistanceSensor distanceRightFront;
+  private DistanceSensor distanceRightBack;
+  //private DistanceSensor distanceFront;
+  private DistanceSensor distanceCarousel;
+  private DistanceSensor distanceIntake;
+  private ColorSensor colorFront;
 
   public void runOpMode() {
 
@@ -31,20 +55,6 @@ public class DropOffMarker extends LinearOpMode {
 	telemetry.update();
 
 	waitForStart();
-	//run during op mode
-	/*
-  1st:
-  Arm: 1056
-	Bucket: -185
-  2nd:
-  Arm: 900
-  Bucket: -89
-  FL: 386
-	FR: -396
-	BL: 391
-	BR: -391
-	*/
-
 	if(opModeIsActive()) {
 		long encoderArmValue;
 		long encoderBucketValue;
@@ -61,81 +71,7 @@ public class DropOffMarker extends LinearOpMode {
     while(bucketTurner.isBusy()||armMotor.isBusy()){
       ;
     }
-    frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-    bucketTurner.setTargetPosition(185);
-		armMotor.setTargetPosition(1056);
-    frontLeft.setTargetPosition(386);
-    frontRight.setTargetPosition(396);
-    backLeft.setTargetPosition(391);
-    backRight.setTargetPosition(391);
-    bucketTurner.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    armMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    frontLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    frontRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    backLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    backRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    bucketTurner.setPower(.5);
-    armMotor.setPower(.5);
-    frontLeft.setPower(1);
-    frontRight.setPower(1);
-    backLeft.setPower(1);
-    backRight.setPower(1);
-    while(bucketTurner.isBusy()||armMotor.isBusy()||frontLeft.isBusy()||frontRight.isBusy()||backLeft.isBusy()||backRight.isBusy()){
-      ;
-    }
-    bucketTurner.setTargetPosition(-98);
-    armMotor.setTargetPosition(900);
-    bucketTurner.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    armMotor.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-    bucketTurner.setPower(.5);
-    armMotor.setPower(.5);
-    while(bucketTurner.isBusy()||armMotor.isBusy()){
-      ;
-    }
-    /*
-    frontLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-		frontRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-		backLeft.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-		backRight.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-		frontLeft.setPower(1);
-		frontRight.setPower(1);
-		backLeft.setPower(1);
-		backRight.setPower(1);
-    */
-    sleep(5000);
-		/*
-		//Used to see the Encoder Values easy
-		while(true){
-			encoderArmValue = armMotor.getCurrentPosition();
-			encoderBucketValue = bucketTurner.getCurrentPosition();
-			encoderFrontLeftValue = frontLeft.getCurrentPosition();
-			encoderFrontRightValue = frontRight.getCurrentPosition();
-			encoderBackLeftValue = backLeft.getCurrentPosition();
-			encoderBackRightValue = backRight.getCurrentPosition();
-			telemetry.addData("Arm Encoder Value", + encoderArmValue);
-			telemetry.addData("Bucket Encoder Value", + encoderBucketValue);
-			telemetry.addData("Front Left Encoder Value", + encoderFrontLeftValue);
-			telemetry.addData("Front Right Encoder Value", + encoderFrontRightValue);
-			telemetry.addData("Back Left Encoder Value", + encoderBackLeftValue);
-			telemetry.addData("Back Right Encoder Value", + encoderBackRightValue);
-			telemetry.update();
-		}
-		*/
-
-		//sleep(2000);
-		/*
-		encoderStartValue = armMotor.getCurrentPosition();
-		armMotor.setVelocity(2000);
-		sleep(500);
-		encoderEndValue = armMotor.getCurrentPosition();
-		armMotor.setVelocity(0);
-		telemetry.addData("Encoder Start", + encoderStartValue);
-		telemetry.addData("Encoder End", + encoderEndValue);
-		telemetry.update();
-		*/
+		sleep(5000);
 	}
   }
 
@@ -171,75 +107,74 @@ public class DropOffMarker extends LinearOpMode {
   }
 
   private void moveArmUp(double power, int time) {
-	armMotor.setPower(power);
-	sleep(time);
-	stopAll();
+    armMotor.setPower(power);
+    sleep(time);
+    stopAll();
   }
   private void moveArmDown(double power, int time) {
-	armMotor.setPower(power*-1);
-	sleep(time);
-	stopAll();
+    armMotor.setPower(power*-1);
+    sleep(time);
+    stopAll();
   }
+
   private void moveForward ( double power, int time){
-	frontRight.setPower(power);
-	frontLeft.setPower(power);
-	backRight.setPower(power);
-	backLeft.setPower(power);
-	sleep(time);
-	stopAll();
+    frontRight.setPower(power);
+    frontLeft.setPower(power);
+    backRight.setPower(power);
+    backLeft.setPower(power);
+    sleep(time);
+    stopAll();
   }
+
   private void moveBack ( double power, int time){
-	frontRight.setPower(power * -1);
-	frontLeft.setPower(power * -1);
-	backRight.setPower(power * -1);
-	backLeft.setPower(power * -1);
-	sleep(time);
-	stopAll();
+    frontRight.setPower(power * -1);
+    frontLeft.setPower(power * -1);
+    backRight.setPower(power * -1);
+    backLeft.setPower(power * -1);
+    sleep(time);
+    stopAll();
   }
+
   private void moveLeft ( double power, int time){
-	frontRight.setPower(power);
-	backRight.setPower(power * -1);
-	frontLeft.setPower(power * -1);
-	backLeft.setPower(power);
-	sleep(time);
-	stopAll();
+    frontRight.setPower(power);
+    backRight.setPower(power * -1);
+    frontLeft.setPower(power * -1);
+    backLeft.setPower(power);
+    sleep(time);
+    stopAll();
   }
+
   private void moveRight ( double power, int time){
-	frontRight.setPower(power * -1);
-	backRight.setPower(power);
-	frontLeft.setPower(power);
-	backLeft.setPower(power * -1);
-	sleep(time);
-	stopAll();
+    frontRight.setPower(power * -1);
+    backRight.setPower(power);
+    frontLeft.setPower(power);
+    backLeft.setPower(power * -1);
+    sleep(time);
+    stopAll();
   }
+
   private void turnLeft ( double power, int time){
-	backRight.setPower(power * -1);
-	frontRight.setPower(power * -1);
-	frontLeft.setPower(power);
-	backLeft.setPower(power);
-	sleep(time);
+    backRight.setPower(power * -1);
+    frontRight.setPower(power * -1);
+    frontLeft.setPower(power);
+    backLeft.setPower(power);
+    sleep(time);
   }
+
   private void turnRight ( double power, int time){
-	frontRight.setPower(power);
-	backRight.setPower(power);
-	frontLeft.setPower(power * -1);
-	backLeft.setPower(power * -1);
-	sleep(time);
-	stopAll();
+    frontRight.setPower(power);
+    backRight.setPower(power);
+    frontLeft.setPower(power * -1);
+    backLeft.setPower(power * -1);
+    sleep(time);
+    stopAll();
   }
+
   private void stopAll () {
-	frontRight.setPower(0);
-	frontLeft.setPower(0);
-	backRight.setPower(0);
-	backLeft.setPower(0);
-	armMotor.setPower(0);
+    frontRight.setPower(0);
+    frontLeft.setPower(0);
+    backRight.setPower(0);
+    backLeft.setPower(0);
+    armMotor.setPower(0);
   }
 }
-/*
-Get Encoder Values
-while(true){
-  _ = _.getCurrentPosition();
-  telemetry.addData("_ Value", _);
-  telemetry.update();
-}
-*/
